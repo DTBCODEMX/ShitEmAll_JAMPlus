451a9197599220df620adddcf8470d298e510b30
y1z <whyis2cool@gmail.com>
2022-10-15 19:48:15 -0500

    Agregando todos los script que hice hasta ahora.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody = null;
+    [SerializeField] private Joystick _joystick;
+    [SerializeField] private GameObject _shit_starting_location = null;
+    [SerializeField] private GameObject _shit_prefab = null;
+    
+    public float speed = 2.00f;
+    private bool _do_flap = false;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.gravityScale = 0.0f;
+        Debug.Assert(_joystick,"joystick needs to be asigned a joysick" );
+        Debug.Assert(_shit_starting_location,"_shit_starting_location needs to be asigned a gameobject" );
+        Debug.Assert(_shit_prefab,"_shit_prefab needs to be asigned a prefab" );
+        
+    }
+
+    void Update()
+    {
+        float vertical_input = _joystick.Vertical;
+        float horizonatal_input = _joystick.Horizontal;
+        
+        transform.Translate(Vector3.right * (horizonatal_input * speed * Time.deltaTime));
+        transform.Translate(Vector3.up * (vertical_input * speed * Time.deltaTime));
+    }
+
+    public void shit()
+    {
+       GameObject go = Instantiate(_shit_prefab, _shit_starting_location.transform);
+       
+    }
+}
