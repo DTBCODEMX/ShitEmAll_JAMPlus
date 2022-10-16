8063343b4a41d977e122cc80d5cb0e7b1bb31aa2
YB <whyis2cool@gmail.com>
2022-10-16 07:42:54 -0500

    Player now die after touching anything with the enemies layer mask

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,11 +9,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody = null;
     [SerializeField] private Joystick _joystick;
-//    [SerializeField] private GameObject _shit_starting_location = null;
-//    [SerializeField] private GameObject _shit_prefab = null;
     [SerializeField] private Animator _animator;
+    [SerializeField] private LayerMask _layer_mask;
+    [SerializeField] private BoxCollider2D _box_collider;
     private static int horizontal_speed_id = Animator.StringToHash("PlayerSpeedHorizontal");
     public float speed = 2.00f;
+    public bool is_dead = false;
 
     void Start()
     {
@@ -21,6 +22,9 @@ public class PlayerController : MonoBehaviour
         _rigidbody.gravityScale = 0.0f;
         Debug.Assert(_joystick,"joystick needs to be asigned a joysick" );
         _animator = GetComponent<Animator>();
+        _layer_mask = 1 << LayerMask.NameToLayer("Enemies");
+        _box_collider = GetComponent<BoxCollider2D>();
+        is_dead = false;
     }
 
     void Update()
@@ -30,6 +34,9 @@ public class PlayerController : MonoBehaviour
         
         transform.Translate(Vector3.right * (horizonatal_input * speed * Time.deltaTime));
         transform.Translate(Vector3.up * (vertical_input * speed * Time.deltaTime));
+
+       handle_collision(); 
+        
 
         _animator.SetFloat("PlayerSpeedHorizontal", MathF.Abs(horizonatal_input));
         
@@ -59,7 +66,16 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
     }
-    
+
+    private void handle_collision()
+    {
+        if (!Physics2D.OverlapBox(transform.position, _box_collider.size, 0.0f, _layer_mask))
+        {
+            return;
+        }
+        print("The dude got me");
+        gameObject.SetActive(false);
+    }
     
     
 }
