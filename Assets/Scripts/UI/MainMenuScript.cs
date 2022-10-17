e0136894ce7a69388775848230ab7593d655bd61
y1z <whyis2cool@gmail.com>
2022-10-16 21:53:44 -0500

    The main menu works.

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -5,15 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject panel1 = null;
+    public GameObject panel2 = null;
+    private Transform starting_pos;
+    private Transform other_pos;
     void Start()
     {
-        
+        Debug.Assert(panel1 != null, "assign an object to this thing");
+        Debug.Assert(panel2 != null, "assign an object to this thing");
+        starting_pos = panel1.transform;
+        other_pos = panel2.transform;
+    }
+    
+    public void OnCredits()
+    {
+        SceneManager.LoadScene("Credit");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSettings()
     {
-        
+        panel1.transform.Translate( Vector3.right * 500000000.0f );
+        panel2.transform.position = starting_pos.position;
     }
 
     public void OnQuit()
